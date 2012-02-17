a3a189a934f6288aec2ca311b6bf746847cfce19
Kyle Peatt <kyle.peatt@tagga.com>
2012-02-17 15:26:49 -0800

    Inputs are now validated against the answer string on the fly… Need to obfuscate answer string so people can't cheat.

--- www/index.php
+++ www/index.php
@@ -1,8 +1,8 @@
 <?php 
 
-$fh = fopen("/Users/kyle/HTML-Crosswords/www/puzzles/classic120217.puz", "rb");
+$fh = fopen("puzzles/classic120217.puz", "rb");
 
-$data = fread($fh, filesize("/Users/kyle/HTML-Crosswords/www/puzzles/classic120217.puz"));
+$data = fread($fh, filesize("puzzles/classic120217.puz"));
 fclose($fh);
 
 $dimdata = unpack("c2dim", substr($data, 0x2C, 2));
@@ -13,7 +13,7 @@ $height = $dimdata['dim2'];
 $answerstring = substr($data, 0x34, $width*$height); //Find the answers string
 
 for ($i = 0; $i <= $height-1; $i++) { // Make a 2d array of answers
-	$answergrid[$i] = preg_split('//', substr($answerstring, $i*$width, $width));
+	$answergrid[$i] = str_split(substr($answerstring, $i*$width, $width));
 }
 
 $bwstring = substr($data, 0x34+$width*$height, $width*$height); //Find the crossword structure
@@ -160,6 +160,20 @@ for ($i = 1; $i <= $height; $i++) {
 			font-size: 18px;
 			text-transform:uppercase;
 		}
+		
+		input.answer:focus {
+			background: lightCyan;
+			outline: 1px solid lightCyan;
+			outline-offset: 0;
+		}
+		
+		input.right {
+			background: paleGreen;
+		}
+		
+		input.wrong {
+			background: salmon;
+		}
 	
 	</style>
 	
@@ -170,6 +184,28 @@ for ($i = 1; $i <= $height; $i++) {
 	<script type="text/javascript">
 		$(function(){
 			$(':input').autotab_magic();
+			
+			var answerKey = "<?php echo $answerstring; ?>";
+			
+			$('input.answer').blur(function() {
+				
+				var currentCell = $(this).attr('id').substr(5);
+				var response = $(this).val();
+				var answer = answerKey.charAt(currentCell-1).toLowerCase();
+				
+				console.log(response);
+				
+				console.log(answer);
+				
+				if (response === answer) {
+					$(this).removeClass('wrong').addClass('right');
+				} else if (response === '') {
+					$(this).removeClass('wrong').removeClass('right');
+				} else {
+					$(this).removeClass('right').addClass('wrong');
+				}
+				
+			});
 			
 			$('a.show').click(function() {
 				$('#answerkey').toggle();
@@ -182,7 +218,7 @@ for ($i = 1; $i <= $height; $i++) {
 
 <body>
 
-<?php echo $answerstring; ?>
+<?php //echo '<pre>';print_r($answergrid);echo '</pre>'; ?>
 
 <h1><?php echo $header['title']; ?></h1>
 <p><?php echo $header['author'].', '.$header['copyright']; ?></p>
@@ -225,6 +261,8 @@ for ($i = 1; $i <= $height; $i++) {
 	
 		$html = "<table>";
 		
+		$k = 1; //For the cell count
+		
 		for ($i = 1; $i <= $height; $i++) {
 
 			$html .= "\n\t<tr>";
@@ -233,10 +271,13 @@ for ($i = 1; $i <= $height; $i++) {
 			
 				if ($numgrid[$i][$j] == -1){ //It's a black square!
 					$html .= "\n\t\t<td class='black'></td>";
+					$k++;
 				} else if ($numgrid[$i][$j] > 0) { // It's a clue!
-					$html .= "\n\t\t<td class='space'><div class='wrapper'><div class='number'>".$numgrid[$i][$j]."</div><input type='text' class='answer' maxlength='1'></div></td>";
+					$html .= "\n\t\t<td class='space'><div class='wrapper'><div class='number'>".$numgrid[$i][$j]."</div><input type='text' class='answer' maxlength='1' rel='[".$i."][".$j."]' id='cell_".$k."'></div></td>";
+					$k++;
 				} else { // It's a blank square!
-					$html .= "\n\t\t<td class='space'><div class='wrapper'><input type='text' class='answer' maxlength='1'></div></td>";
+					$html .= "\n\t\t<td class='space'><div class='wrapper'><input type='text' class='answer' maxlength='1' rel='[".$i."][".$j."]' id='cell_".$k."'></div></td>";
+					$k++;
 				}
 			}
 			
@@ -267,9 +308,9 @@ for ($i = 1; $i <= $height; $i++) {
 				if ($numgrid[$i][$j] == -1){ //It's a black square!
 					$html .= "\n\t\t<td class='black'></td>";
 				} else if ($numgrid[$i][$j] > 0) { // It's a clue!
-					$html .= "\n\t\t<td class='space'><div class='number'>".$numgrid[$i][$j]."</div><span class='answer'>".$answergrid[$i][$j+1]."</span></td>";
+					$html .= "\n\t\t<td class='space'><div class='wrapper'><div class='number'>".$numgrid[$i][$j]."</div><span class='answer'>".$answergrid[$i][$j]."</span></div></td>";
 				} else { // It's a blank square!
-					$html .= "\n\t\t<td class='space'><span class='answer'>".$answergrid[$i][$j+1]."</span></td>";
+					$html .= "\n\t\t<td class='space'><div class='wrapper'><span class='answer'>".$answergrid[$i][$j]."</span></div></td>";
 				}
 			}
 			
