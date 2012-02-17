dc27bfd15a892451e6eec9db99624c9789bb0d4b
Kyle Peatt <kyle.peatt@tagga.com>
2012-02-16 19:00:42 -0800

    Have it rendering the PUZ file properly and gathering header/clues arrays
    Still need to figure out how to translate clues into clue numbers and I get the feeling I don't have all of the clues...

--- www/index.php
+++ www/index.php
@@ -1,11 +1,44 @@
 <?php 
 
-$width = 15;
-$height = 15;
+$fh = fopen("/Users/kyle/HTML-Crosswords/www/puzzles/cs120216.puz", "rb");
 
-$fill = "-----.----.---------.----.----------------------.-----.-----...---...---...--------------------.-----.-------.-----.-------.-----.--------------------...---...---...-----.-----.----------------------.----.---------.----.-----";
+$data = fread($fh, filesize("/Users/kyle/HTML-Crosswords/www/puzzles/cs120216.puz"));
+fclose($fh);
 
-$cells = str_split($fill);
+$dimdata = unpack("c2dim", substr($data, 0x2C, 2));
+
+$width = $dimdata['dim1'];
+$height = $dimdata['dim2'];
+
+$answerstring = substr($data, 0x34, $width*$height); //Find the answers string
+
+for ($i = 0; $i <= $height-1; ++$i) { // Make a 2d array of answers
+	$answergrid[$i] = preg_split('//', substr($answerstring, $i*$width, $width));
+}
+
+$bwstring = substr($data, 0x34+$width*$height, $width*$height); //Find the crossword structure
+
+for ($i = 0; $i < $height-1; ++$i) { // Make a 2d array of structure
+	$bwgrid[$i] = preg_split('//', substr($bwstring, $i*$width, $width));
+}
+
+$cluestring = substr($data, 0x34+$width*$height+$width*$height);
+
+$newclues = preg_split('/\0/', $cluestring);
+
+$header = array("title" => array_shift($newclues), "author" => array_shift($newclues), "copyright" => array_shift($newclues));
+
+for ($i = 0; $i < count($bwgrid); ++$i) { // 2d Array of Clue Numbers
+	for ($j = 0; $j < count($bwgrid[$i]); ++$j) {
+		$numgrid[$i][$j] = $bwgrid[$i][$j];
+	}
+}
+
+echo "<pre>";
+print_r($header);
+echo "</pre>";
+
+$cells = str_split($bwstring);
 
 ?>
 
