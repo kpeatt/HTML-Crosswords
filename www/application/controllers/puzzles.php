24bd934bb01919ac70318889db2bd924aab61a6a
Kyle Peatt <kyle.peatt@tagga.com>
2012-04-01 11:02:09 -0700

    Add Typography helper to render clues better

--- www/application/controllers/puzzles.php
+++ www/application/controllers/puzzles.php
@@ -6,6 +6,7 @@ class Puzzles extends CI_Controller {
 		parent::__construct();
 		$this->load->model('puzzles_model');
 		$this->load->helper('json'); 
+		$this->load->library('typography');
 	}
 	
 	public function index()	{
@@ -16,6 +17,12 @@ class Puzzles extends CI_Controller {
 	
 		$data['puzzles'] = $this->puzzles_model->get_puzzle();
 		$data['title'] = 'Puzzles Collection';
+		
+		$i = 0; //Convert Regular quotes to Smart Quotes
+		foreach ($data['puzzles'] as $puzzle) {
+			$data['puzzles'][$i]['meta']['title'] = $this->typography->format_characters($puzzle['meta']['title']);
+			$i++;
+		}
 		
 		$data['puzzle'] = '';
 		
@@ -40,7 +47,22 @@ class Puzzles extends CI_Controller {
 		$data['puzzle']['across'] = json_decode($data['puzzle']['across'], true);
 		$data['puzzle']['down'] = json_decode($data['puzzle']['down'], true);
 		
-		$data['title'] = $data['puzzle']['meta']['title'];
+		$i = 0; //Convert Regular quotes to Smart Quotes
+		foreach ($data['puzzle']['across'] as $across) {
+			$data['puzzle']['across'][$i]['cluetext'] = $this->typography->format_characters($across['cluetext']);
+			$i++;
+		}
+		$i = 0;
+		foreach ($data['puzzle']['down'] as $down) {
+			$data['puzzle']['down'][$i]['cluetext'] = $this->typography->format_characters($down['cluetext']);
+			$i++;
+		}
+		
+		$data['puzzle']['meta']['title'] = $this->typography->format_characters($data['puzzle']['meta']['title']);
+		$data['puzzle']['meta']['copyright'] = $this->typography->format_characters($data['puzzle']['meta']['copyright']);
+		$data['puzzle']['meta']['author'] = $this->typography->format_characters($data['puzzle']['meta']['author']);
+		
+		$data['title'] = $this->typography->format_characters($data['puzzle']['meta']['title']);
 										
 		$data['js'] = array('jquery', 'bootstrap', 'crosswordview');	
 		$data['css'] = array('common', 'bootstrap');
